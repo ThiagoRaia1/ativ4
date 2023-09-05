9b07ddbefa4aecd20902291f83ce3e34138cac52
Thiago Raia <thiagoraia2004@gmail.com>
2023-09-04 21:58:21 -0300

    Adição da funcionalidade para comparar números

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import PromptSync from "prompt-sync";
 const prompt = PromptSync()
 
+let opcao = 0
+
 function maiorNumero() {
     const num1 = Number(prompt("Digite um número: "))
     const num2 = Number(prompt("Digite o segundo número: "))
     const num3 = Number(prompt("Digite o terceiro número: "))
+    if (num1 > num2 && num3) {
+        console.log(`O número ${num1} é o maior entre os 3 digitados\n`)
+    } else if (num2 > num1 && num3) {
+        console.log(`O número ${num2} é o maior entre os 3 digitados\n`)
+    } else if (num3 > num1 && num2) {
+        console.log(`O número ${num3} é o maior entre os 3 digitados\n`)
+    }
 }
-
-console.log("1 - Comparar números")
-let opcao = Number(prompt("Escolha uma opção: "))
-if (opcao == 1) {
-    maiorNumero()
-}
+do {
+    console.log("1 - Comparar números")
+    opcao = Number(prompt("Escolha uma opção: "))
+    switch (opcao) {
+        case 1: maiorNumero(); break;
+        case 0: console.log(`Programa encerrado`); break;
+        default: console.log(`Opção inválida.`)
+    }
+} while (opcao != 0)
